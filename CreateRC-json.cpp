432986c6c03f56c544d75ce04d3194bbe3efc57f
dondon3345678 <b01501085@localhost.localdomain>
2016-06-03 04:42:53 -0400

    add rc-temp.json

---
+++ CreateRC-json.cpp
@@ -0,0 +1,15 @@
+#include "json/json.h"
+#include <string>
+
+using namespace std;
+
+int main(int argc , const char** argv){
+
+	string image = "jane801030/ticket-test";
+	Json::Value root ;
+	Json::StyledStreamWriter writer;
+	ofstream tempJson("rc-temp.json");	
+
+
+
+}
